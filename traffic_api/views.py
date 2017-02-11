c97d322e97b1c1cec632a2844a647034668f7fff
Bruno Bachmann <bfbachmann@gmail.com>
2017-02-10 16:07:03 -0800

    Remove crsf from receive_data()

--- traffic_api/views.py
+++ traffic_api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def receive_data(request):
     if request.method == 'GET':
         return render(request, 'index.html')
