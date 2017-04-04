1ffa5e5b51ea100958e02910cd235e8a6e8923aa
chadlagore <chad.d.lagore@gmail.com>
2017-04-04 11:33:42 -0700

    add test back in

--- traffic_api/tests.py
+++ traffic_api/tests.py
@@ -8,3 +8,7 @@ from .utils import floor_date
 def test_cars_positive():
     for i in range(1000):
         assert get_cars(0) >= 0
+
+
+def test_floor_date():
+    assert floor_date(1490821240, 'daily') == 1490770800
